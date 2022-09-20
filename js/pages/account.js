b719a51538dd4df6716dcd06bd4906e6a6d99e35
Bruno Cocco <brunoe.cocco@gmail.com>
2022-09-20 19:44:28 -0300

    acoount js

---
+++ js/pages/account.js
@@ -0,0 +1,13 @@
+   
+const clickLogin = document.getElementById("clickFormLogin");
+console.log(clickLogin)
+clickLogin.addEventListener("click", ()=>{
+    console.log("cluck")
+
+    // document.getElementById("email").value;
+    // document.getElementById("new-password").value;
+    
+    // console.log(email)
+    // console.log(password)
+    
+});
